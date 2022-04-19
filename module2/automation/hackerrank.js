76d23d718e06e6cc0c143060c84c2dedc28e37b7
Prabhansh Singh <prabhansh.manchanda@gmail.com>
2022-04-19 23:35:14 +0530

    hackerrank automation pt2

--- module2/automation/hackerrank.js
+++ module2/automation/hackerrank.js
@@ -5,6 +5,18 @@ const pass = 'abcdef';
 const browserPromise = puppeteer.launch({ headless: false, defaultViewport: null });
 let page;
 
+function waitAndClick(selector){
+    return new Promise(function(resolve, reject){
+        let waitPromise = page.waitForSelector(selector);
+        waitPromise.then(function(){
+            let clickPromise = page.click(selector);
+            return clickPromise;
+        }).then(function(){
+            resolve();
+        })
+    })
+}
+
 browserPromise.then(function (browserInstance) {
     console.log("Browser is opened");
     let pagePromise = browserInstance.newPage();
@@ -16,11 +28,7 @@ browserPromise.then(function (browserInstance) {
     return urlPromise;
 }).then(function () {
     console.log("hackerrank is opened");
-    let waitPromise = page.waitForSelector('ul.menu a');
-    return waitPromise;
-}).then(function(){
-    let clickPromise = page.click('ul.menu a');
-    return clickPromise;
+    return waitAndClick('ul.menu a');
 }).then(function(){
     let waitPromise = page.waitForSelector('.fl-module-content.fl-node-content .fl-button');
     return waitPromise;
@@ -45,15 +53,22 @@ browserPromise.then(function (browserInstance) {
     console.log("login successful!");
     return clickPromise;
 }).then(function(){
-    let waitPromise = page.waitForSelector('[data-attr1="algorithms"]');
+    return waitAndClick('[data-attr1="algorithms"]');
+}).then(function(){
+    return waitAndClick("[value='warmup']");
+}).then(function(){
+    let waitPromise = page.waitForSelector(".challenges-list .js-track-click.challenge-list-item");
     return waitPromise;
 }).then(function(){
-    let clickPromise = page.click('[data-attr1="algorithms"]');
-    return clickPromise;
-}).then(function(){
-    let waitPromise = page.waitForSelector("[value='warmup']");
-    return waitPromise;
-}).then(function(){
-    let clickPromise = page.click("[value='warmup']");
-    return clickPromise;
+    let domClickPromise = page.evaluate(function(){
+        let arr = [];
+        let btns = document.querySelectorAll('.challenges-list .js-track-click.challenge-list-item');
+        for(let i = 0; i<btns.length; i++){
+            arr.push(btns[i].href);
+        }
+        return arr;
+    })
+    return domClickPromise;
+}).then(function(quesArr){
+    console.log(quesArr);
 })
