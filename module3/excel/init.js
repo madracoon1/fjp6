c0eeec5eaf07ab1559046deb3e39c2ddc18f421c
Prabhansh Singh <prabhansh.manchanda@gmail.com>
2022-06-09 22:01:47 +0530

    excel completed (except for diy menu options)

--- module3/excel/init.js
+++ module3/excel/init.js
@@ -5,13 +5,13 @@ function initCells() {
 
     cellsContent += "<div class = 'top-row'>";
     for (let i = 0; i < 26; i++) {
-        cellsContent += `<div class = 'top-row-cell'>${String.fromCharCode(65 + i)}</div>`;
+        cellsContent += `<div class = 'top-row-cell' trid = '${i}'>${String.fromCharCode(65 + i)}</div>`;
     }
     cellsContent += "</div>";
 
     cellsContent += "<div class = 'left-col'>";
     for (let i = 0; i < 100; i++) {
-        cellsContent += `<div class = 'left-col-cell'>${i + 1}</div>`;
+        cellsContent += `<div class = 'left-col-cell' lcid = '${i}'>${i + 1}</div>`;
     }
     cellsContent += "</div>";
 
